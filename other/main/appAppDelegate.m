4bbf11ec542dc525d0ae035f26969181eb143d4f
chrisjamesallen <chris@jonny-lu.com>
2014-06-07 17:17:28 +0100

    cleaner code

--- other/main/appAppDelegate.m
+++ other/main/appAppDelegate.m
@@ -1,4 +1,7 @@
 
+/* : so here we want to load up the game engine emma,
+ set window size and we are good to go!
+ =================================================== */
 
 #import "appAppDelegate.h"
 #import "AppGLView.h"
@@ -8,49 +11,51 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    NSWindow * window = self.window;
-
-    // make a view...
-    self.glview = [[AppGLView alloc] initWithFrame: CGRectMake(0,0,512,512)];
-    [self.glview setup];
-    window.delegate = self.glview;
-
-    // make window transparent
-    [window setOpaque:NO];
-    NSColor * transparent = [NSColor colorWithCalibratedWhite:1.0 alpha:0.4];
-    [window setBackgroundColor:transparent];
-    
-    //add view to window
-//    [window.contentView addSubview:self.glview];
-	[window setContentView:self.glview];
-    self.glview->windowSize =  window.frame.size;
-	self.glview.frame = CGRectMake(0, 0, window.frame.size.width, window.frame.size.height);
- 
-    //create Emma
-    self.director = [[[Emma alloc]init] autorelease];
-
-    //start Emma :)
-    //[self.Emma start];
-
-    self.glview.director = self.director;
-      //Make full screen dimensions..
-    //CGRect frame = [NSScreen mainScreen].frame;
-    //[window setFrame: frame display:YES];
-
-
-/*
-* TODO
-* learn screen methods
-* */
-
-
-
-
+  [self configureWindow];
+  [self setupDirector];
 }
 
+-(void)configureWindow{
+  //make translucent
+  [self.window setOpaque:NO];
+  NSColor * transparent = [NSColor colorWithCalibratedWhite:1.0 alpha:0.4];
+  [self.window setBackgroundColor:transparent];
+}
+
+-(void)setupDirector{
+  //vars
+  NSWindow * window = self.window;
+  
+  //create glview
+  self.glview = [[AppGLView alloc] initWithFrame: CGRectMake(0,0,512,512)];
+  [self.glview setup];
+
+  //add to window
+  window.delegate = self.glview;
+  [window setContentView: self.glview];
+  
+  //set view dimensions
+  self.glview->windowSize =  window.frame.size;
+	self.glview.frame = CGRectMake(0, 0, window.frame.size.width, window.frame.size.height);
+  //Make full screen dimensions..
+  //CGRect frame = [NSScreen mainScreen].frame;
+  //[window setFrame: frame display:YES];
+  
+  //create Emma
+  self.director = [[[Emma alloc]init] autorelease];
+  
+  //assign emma to view
+  self.glview.director = self.director;
+  
+}
+
+
+
+
+
 - (void)dealloc {
-    [_director release];
-    [super dealloc];
+  [_director release];
+  [super dealloc];
 }
 
 
