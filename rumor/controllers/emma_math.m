b66d5dce1ee26eeaff8cb6af6dc06cd57629b3b5
chrisjamesallen <chris@jonny-lu.com>
2014-06-26 00:42:07 +0100

    increased performance on mat4, now with global mat4s to reduce malloc overhead (not pushing and popping just yet)

--- rumor/controllers/emma_math.m
+++ rumor/controllers/emma_math.m
@@ -23,11 +23,15 @@ static int mat4_newindex( lua_State* L );
 static int mat4_call( lua_State* L );
 static int mat4_mul( lua_State* L );
 static int mat4_sub( lua_State* L );
-static int mat4_add( lua_State* L );
 static int mat4_gc( lua_State* L );
 static int mat4_projection( lua_State* L );
 static int mat4_scale( lua_State* L );
-
+static int mat4_multiply( lua_State* L );
+static int mat4_subtract( lua_State* L );
+static int mat4_add( lua_State* L );
+static int mat4_translate( lua_State* L );
+static int mat4_identity( lua_State* L );
+static int mat4_assign( lua_State* L );
 
 static const struct luaL_Reg luamat4Lib[] = { { "__index", mat4_index },
                                               { "__newindex", mat4_newindex },
@@ -36,8 +40,13 @@ static const struct luaL_Reg luamat4Lib[] = { { "__index", mat4_index },
                                               { "__sub", mat4_sub },
                                               { "__add", mat4_add },
                                               { "__gc", mat4_gc },
-                                              { "Projection", mat4_projection },
-                                              { "Scale", mat4_scale },
+                                              { "CreateProjection", mat4_projection },
+                                              { "CreateScale", mat4_scale },
+                                              // these assign to own matrix
+                                              { "identity", mat4_identity },
+                                              { "multiply", mat4_multiply },
+                                              { "translate", mat4_translate },
+                                              { "assign", mat4_assign },
                                               { NULL, NULL } };
 
 void lua_initMat4( lua_State* L ) {
@@ -110,14 +119,14 @@ static int mat4_add( lua_State* L ) { return 1; }
 static int mat4_gc( lua_State* L ) {
     // dump shit here
     lua_mat4* mat4 = (lua_mat4*)luaL_checkudata( L, 1, "mat4" );
-    printf( "drop mat4" );
     free( mat4->data );
+    printf( "deleted it" );
     return 1;
 }
 
 
 static int mat4_projection( lua_State* L ) {
-    lua_mat4* mat4;
+    lua_mat4* mat;
     kmScalar fovY, aspect, zNear, zFar;
 
     fovY = lua_tonumberx( L, -4, NULL );
@@ -125,13 +134,15 @@ static int mat4_projection( lua_State* L ) {
     zNear = lua_tonumberx( L, -2, NULL );
     zFar = lua_tonumberx( L, -1, NULL );
 
+
+    mat = (lua_mat4*)lua_newuserdata( L, sizeof( lua_mat4 ) );
     kmMat4* data = (kmMat4*)malloc( sizeof( kmMat4 ) );
-    mat4 = (lua_mat4*)lua_newuserdata( L, sizeof( lua_mat4 ) );
-    mat4->data = data;
-    luaL_getmetatable( L, "mat4" );
-    lua_setmetatable( L, -2 );
+    mat->data = data;
 
     kmMat4PerspectiveProjection( data, fovY, aspect, zNear, zFar );
+
+    luaL_getmetatable( L, "mat4" );
+    lua_setmetatable( L, -2 );
     return 1;
 }
 
@@ -160,16 +171,81 @@ static int mat4_orthoprojection( lua_State* L ) {
 
 static int mat4_scale( lua_State* L ) {
     kmScalar x, y, z;
-    lua_mat4* mat4;
+    lua_mat4* mat1;
+    kmMat4 data;
+
     x = lua_tonumberx( L, -3, NULL );
     y = lua_tonumberx( L, -2, NULL );
     z = lua_tonumberx( L, -1, NULL );
 
-    kmMat4 data; // = (kmMat4*)malloc( sizeof( kmMat4 ) );
-    mat4 = (lua_mat4*)lua_newuserdata( L, sizeof( lua_mat4 ) );
-    mat4->data = &data;
+    mat1 = (lua_mat4*)lua_newuserdata( L, sizeof( lua_mat4 ) );
+    kmMat4Scaling( &data, x, y, z );
+    mat1->data = &data;
+
     luaL_getmetatable( L, "mat4" );
     lua_setmetatable( L, -2 );
-    kmMat4Scaling( &data, x, y, z );
     return 1;
 }
+
+
+static int mat4_identity( lua_State* L ) {
+    float x, y, z;
+    lua_mat4* mat1;
+
+    mat1 = (lua_mat4*)luaL_checkudata( L, 1, "mat4" );
+
+    kmMat4Identity( mat1->data );
+    return 1;
+}
+
+static int mat4_assign( lua_State* L ) {
+    lua_mat4* mat1;
+    lua_mat4* mat2;
+
+    mat1 = (lua_mat4*)luaL_checkudata( L, -2, "mat4" );
+    mat2 = (lua_mat4*)lua_touserdata( L, -1 );
+
+    kmMat4Assign( mat1->data, mat2->data );
+
+    lua_pushvalue( L, -2 );
+    return 1;
+}
+
+static int mat4_translate( lua_State* L ) {
+    float x, y, z;
+    kmMat4 translation;
+    lua_mat4* mat1;
+
+    x = lua_tonumberx( L, -3, NULL );
+    y = lua_tonumberx( L, -2, NULL );
+    z = lua_tonumberx( L, -1, NULL );
+    mat1 = (lua_mat4*)luaL_checkudata( L, 1, "mat4" );
+
+    kmMat4Translation( &translation, x, y, z );
+    kmMat4Multiply( mat1->data, mat1->data, &translation );
+    return 1;
+}
+
+
+static int mat4_multiply( lua_State* L ) {
+    lua_mat4* mat1;
+    lua_mat4* mat2;
+    kmMat4 scaling;
+
+    mat1 = (lua_mat4*)luaL_checkudata( L, -2, "mat4" );
+
+    if ( lua_isuserdata( L, -1 ) ) {
+        mat2 = (lua_mat4*)lua_touserdata( L, -1 );
+
+        kmMat4Multiply( mat1->data, mat1->data, mat2->data );
+
+    } else if ( lua_isnumber( L, -1 ) ) {
+        float scale = lua_tonumberx( L, -1, NULL );
+        kmMat4Scaling( &scaling, scale, scale, scale );
+        kmMat4Multiply( mat1->data, mat1->data, &scaling );
+    }
+
+    // as we self assigned the left hand matrix, push for result
+    lua_pushvalue( L, -2 );
+    return 1;
+};
--- rumor/kazmath/GL/mat4stack.h
+++ rumor/kazmath/GL/mat4stack.h
@@ -29,20 +29,20 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "../mat4.h"
 
 typedef struct km_mat4_stack {
-	int capacity; /*The total item capacity*/
-	int item_count; /*The number of items*/
-	kmMat4* top;
-	kmMat4* stack;
+    int capacity;   /*The total item capacity*/
+    int item_count; /*The number of items*/
+    kmMat4* top;
+    kmMat4* stack;
 } km_mat4_stack;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void km_mat4_stack_initialize(km_mat4_stack* stack);
-void km_mat4_stack_push(km_mat4_stack* stack, const kmMat4* item);
-void km_mat4_stack_pop(km_mat4_stack* stack, kmMat4* pOut);
-void km_mat4_stack_release(km_mat4_stack* stack);
+void km_mat4_stack_initialize( km_mat4_stack* stack );
+void km_mat4_stack_push( km_mat4_stack* stack, const kmMat4* item );
+void km_mat4_stack_pop( km_mat4_stack* stack, kmMat4* pOut );
+void km_mat4_stack_release( km_mat4_stack* stack );
 
 #ifdef __cplusplus
 }
