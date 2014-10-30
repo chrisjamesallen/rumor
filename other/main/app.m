12bd876e4bc3b4b2c57ae2a90d0023e4b508c350
chrisjamesallen <chris@jonny-lu.com>
2014-10-30 21:34:53 +0000

    webview

--- other/main/app.m
+++ other/main/app.m
@@ -5,7 +5,6 @@
 
 
 #import "app.h"
-#import "AppGLView.h"
 #import "Emma.h"
 #import <WebKit/WebKit.h>
 
@@ -13,7 +12,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     [self configureWindow];
-   // [self setupDirector];
+    // [self setupDirector];
 }
 
 - (void)configureWindow {
@@ -25,10 +24,11 @@
     // frame = CGRectMake( 0, 0, 500, 500 );
     //[self.window setFrame:frame display:YES];
     [self.window setLevel:kCGFloatingWindowLevelKey];
-    
-    
-    WebView * view = [[WebView alloc] initWithFrame: self.window.frame frameName:@"" groupName:@""];
-    
+
+
+    WebView *view =
+        [[WebView alloc] initWithFrame:self.window.frame frameName:@"" groupName:@""];
+
     NSURL *url = [NSURL URLWithString:@"http://localhost:3000"];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
     [[view mainFrame] loadRequest:urlRequest];
@@ -36,37 +36,10 @@
     [self.window setIgnoresMouseEvents:YES];
     [view setDrawsBackground:NO];
     // = [UIColor clearColor];
-    
-    
+
     // todo reapply frame
     [self.window setLevel:kCGDesktopWindowLevelKey];
-   //[self.window setFrame:[[NSScreen mainScreen] frame] display:YES];
-}
-
-- (void)setupDirector {
-
-    // vars
-    NSWindow *window = self.window;
-
-    // create glview
-    self.glview = [[AppGLView alloc] initWithFrame:self.window.frame];
-    [self.glview setup];
-
-    // add to window
-    window.delegate = self.glview;
-    [window setContentView:self.glview];
-
-    // set view dimensions
-    self.glview->windowSize = window.frame.size;
-
-    // create Emma
-    self.emma = [[[Emma alloc] init] autorelease];
-    self.emma->view = self.glview;
-    // assign emma to view
-    self.glview.director = self.emma;
-
-    // start emma
-    //[self.emma start];
+    [self.window setFrame:[[NSScreen mainScreen] frame] display:YES];
 }
 
 - (void)dealloc {
